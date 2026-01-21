9ac20a7c68c50bd8d16b7ea1648e5e782351d82c
Leidson Gonçalves <leidsonpc@gmail.com>
2026-01-21 16:00:37 -0300

    Aula06

---
+++ Aula4/index.js
@@ -0,0 +1,7 @@
+console.log('Hello World!!'); //Comentário inline
+console.log('Meu primeiro programa em JavaScript');
+console.log('Acordei às', 10, 'da manhã');
+console.log('Fui tomar café às', 10.30);
+console.log('Comecei a trabalhar às', 11);
+console.log('Almocei às', 13);
+//Comentário inline
---
+++ Aula6/index.js
@@ -0,0 +1,7 @@
+let nome = 'João';
+
+console.log('João nasceu em 2005'); 
+console.log('Maria nasceu em 2007');
+console.log('João e Maria são irmãos');
+console.log('Maria gosta de estudar matemática');
+console.log('João gosta de jogar futebol');
