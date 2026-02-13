1ad7e338df12e2664d4c45cafe1767d805e3dd14
Leidson Gonçalves <leidsonpc@gmail.com>
2026-02-13 10:58:17 -0300

    JS-Basic completed

---
+++ 01 - Aulas - JavaScript Básico/Aula19/index.js
@@ -0,0 +1,7 @@
+ /*
+   • Primitivos (imutáveis) - string, number, boolean, undefined e null (bigint e symbol) - Valor copiados
+
+   • Referência (mutável) - array, object e function - Passados por referência da memória
+   (Para copiar "*...VARIAVEL)")
+ */
+ 
